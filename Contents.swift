54f8a5075cb3ec9a890049a1e2a72adba5f746ac
Apple <apple@Apples-MacBook-Pro-4.local>
2019-07-22 14:02:21 -0500

    Day One Tests

---
+++ Contents.swift
@@ -0,0 +1,155 @@
+/*
+
+import Cocoa
+
+var str = "Hello, playground" //created a string that said "Hello, Playground"
+/*
+string, integer, double, interpolation, variable, constant, keyword
+
+string is basically things to put intbetween quoatation marks like words or numbers or symbols "xyz"
+ 
+ integers are non-decimal numbers
+ 
+ 
+*/
+
+
+////////////
+
+
+var address = "1313 Mockbird Lane" // declared a variable for address
+
+address = "254 Ocean Avenue" // redeclared the variable address because you moved
+
+let name = "Ellie" //only used for permanent objects; uses less memory than variables
+
+
+/////////
+
+
+// Quiz Time!
+
+var string = 5 // data type is a integer or a double! (this defaults to a double tho)
+var test = "5" // data type is a string"
+
+var new = string // string is a variable you declared above, so basically new = 5
+// quiz done
+
+var empty : Double // this is basically a variable that the user inputs like a calculator, data type needs to be capitalized!!!!
+
+///////////
+
+/*
+ 
+//printing a string//
+var printMe = "Hello World"
+
+print (printMe)
+
+
+var firstName = "Ellie"
+var lastName = "Slawek"
+
+print (firstName, lastName)
+print (firstName + " " + lastName)
+
+*/
+
+
+///////
+
+// interpolation sample
+let apples = 4
+print ("i have \(apples) apples")
+
+
+///////////
+
+
+/*
+ make a program that uses string interpolation to print a math problem
+ */
+
+print ("9+10 = \(9+10)")
+
+
+
+///////////
+
+
+/*
+ problem with the numbers 9.7 and -3
+ */
+
+print ("9.7 + -3 = \(9.7 + -3)")
+
+var num : Float
+num = 9.7-3
+print ("Rounded the answer is\(num)")
+
+
+
+/////////////
+/* matts LIFE if statements
+
+if statements
+
+
+var temp = 60
+
+if temp > 80 {
+    print ("wear shorts")
+} else {
+   print ("wear pants")
+}
+ 
+ */
+
+
+//////////////////
+
+
+
+/* if it is 87 print out I guess correct, in not print its not 87
+ */
+var temp2 = 87
+if temp2 == 87 {
+    print ("I guessed right")
+}else{
+    print ("It's not 87")
+}
+
+
+var temp3 = 97
+if !(temp3 == 87){
+    print ("woohoo")
+}
+
+
+
+
+/////////
+
+
+// If statements
+
+var favoriteFood = "McDonalds"
+
+if favoriteFood == "chipotle" {
+    print ("you must like chipotle")
+}
+
+if favoriteFood == "Starbucks" {
+    print ("You must like Starbucks")
+} else if favoriteFood == "McDonalds" {
+    print ("You really like McDonalds")
+} else {
+    print ("You must like \(favoriteFood) more than Starbucks")
+}
+
+ 
+ */
+
+
+
+
